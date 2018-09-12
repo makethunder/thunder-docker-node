d6f6ff9f4c96bbeefecebb98cc2eaaa9bc04ca44
Kian Mohageri <kian.mohageri@gmail.com>
2018-09-11 17:03:13 -0700

    Initial commit
    Docker base/parent image based on official Node images; adding chamber and other
    dependencies for AWS deployments.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NODE_RELEASES="6 8 10"
+
+for release in $NODE_RELEASES; do
+    for variant in $(ls "${release}"); do
+        docker build \
+            -t makethunder/node:${release}-${variant} \
+            -f ${release}/${variant}/Dockerfile .
+    done
+done
---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Custom entrypoint that, when required, uses chamber 'exec' to run our
+# application.
+#
+# Chamber populates environment variables based on key/value pairs in AWS
+# Parameter Store.
+#
+# Requires Chamber and JQ (should be baked into Docker image)
+
+METADATA_URL=http://169.254.170.2/v2/metadata
+CHAMBER=/usr/local/bin/chamber
+
+if [ ! -z "${AWS_REGION}" ]; then
+    # Identifies our task name (e.g., worker-staging, worker-production) which
+    # maps to our service's Parameter Store path.
+    FAMILY=$(curl --silent --show-error "${METADATA_URL}" | jq -r .Family)
+
+    exec "${CHAMBER}" exec "${FAMILY}" -- "$@"
+else
+    exec "$@"
+fi
