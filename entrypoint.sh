5975e420a6d464c973e31c22b879ed49ecaf075f
Kian Mohageri <kian.mohageri@gmail.com>
2018-10-09 14:03:43 -0700

    Allow users to specify custom secrets path
    By default, the entrypoint looks for secrets in the path based on task
    name/family (e.g., adserver-production).  Sometimes users want to
    override this, so this patch adds support for the env var CHAMBER_PATH
    to do so.
    
    PL-28450

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,13 @@ METADATA_URL="http://169.254.170.2/v2/metadata"
 CHAMBER="/usr/local/bin/chamber"
 
 if [ ! -z "${AWS_REGION}" ]; then
-    # Identifies our task name (e.g., worker-staging, worker-production) which
-    # maps to our service's Parameter Store path.
-    FAMILY=$(curl --silent --show-error "${METADATA_URL}" | jq -r .Family)
+    if [ -z "${CHAMBER_PATH}" ]; then
+        # Identifies our task name (e.g., worker-staging, worker-production)
+        # which maps to our service's Parameter Store path.
+        CHAMBER_PATH=$(curl --silent --show-error "${METADATA_URL}" | jq -r .Family)
+    fi
 
-    exec "${CHAMBER}" exec "${FAMILY}" -- "$@"
+    exec "${CHAMBER}" exec "${CHAMBER_PATH}" -- "$@"
 else
     exec "$@"
 fi
